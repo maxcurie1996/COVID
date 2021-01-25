a953a63b19190f3fecb3c868caac7c442386d42f
maxcurie1996 <47543965+maxcurie1996@users.noreply.github.com>
2021-01-25 09:26:30 -0600

    1

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from Parameter_data import *
-#from Parameter_data import cross_section
-
-#https://www.statista.com/statistics/241488/population-of-the-us-by-sex-and-age/
-
-x_list=np.arange(-30,30,0.1)
-y_list=np.arange(-30,30,0.1)
-
-print(np.corrcoef(x_list,y_list))
